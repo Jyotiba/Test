f1b57ce0b67147a9e528079210e084a6ea59d666
Jyotiba <jjyotiba24sd@gmail.com>
2021-10-10 15:29:29 +0530

    New File Created

---
+++ test.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Sunday 10 October 2021 03:25:58  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jyotiba S (), jjyotiba24sd@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main(int argc,char*argv[])
+{
+
+	return 0;
+}
